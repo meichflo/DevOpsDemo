333f1e2255804fb8c53cbd3e9a92df7624271a71
meichflo <meichflo@students.zhaw.ch>
2024-03-30 12:46:54 +0100

    init StringCalculatorTest

---
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package ch.zhaw.iwi.devops.demo;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void testAdd() {
+        StringCalculator calculator = new StringCalculator();
+
+        assertEquals(0, calculator.add(""));
+        assertEquals(1, calculator.add("1"));
+        assertEquals(3, calculator.add("1,2"));
+    }
+}
