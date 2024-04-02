57e85085ba50bea348fa61689d4e04e835eba7db
meichflo <meichflo@students.zhaw.ch>
2024-04-02 07:31:29 +0200

    Erweiterung vom Test für newLine

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/StringCalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/StringCalculatorTest.java
@@ -13,5 +13,6 @@ public class StringCalculatorTest {
         assertEquals(1, calculator.add("1"));
         assertEquals(3, calculator.add("1,2"));
         assertEquals(10, calculator.add("1,2,3,4"));
+        assertEquals(6, calculator.add("1\n2,3"));
     }
 }
