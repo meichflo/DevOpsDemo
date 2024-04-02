4f4109fb2908f22f0a564f591c3d446135ead1bc
meichflo <meichflo@students.zhaw.ch>
2024-04-02 07:40:56 +0200

    adapt the funcition fpr dynamic deliminators

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/StringCalculator.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/StringCalculator.java
@@ -6,7 +6,15 @@ public class StringCalculator {
             return 0;
         }
 
-        String[] parts = numbers.split(",|\n");
+        String delimiter = ",|\n";
+        if (numbers.startsWith("//")) {
+            int delimiterStart = numbers.indexOf("//") + 2;
+            int delimiterEnd = numbers.indexOf("\n");
+            delimiter = numbers.substring(delimiterStart, delimiterEnd);
+            numbers = numbers.substring(delimiterEnd + 1);
+        }
+
+        String[] parts = numbers.split(delimiter);
         int sum = 0;
         for (String part : parts) {
             sum += Integer.parseInt(part);
