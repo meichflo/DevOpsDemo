e13a3fac0b508f0a76db048397fd586af204ef8d
meichflo <meichflo@students.zhaw.ch>
2024-04-02 07:32:04 +0200

    anpassung von Methode für NewLine

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/StringCalculator.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/StringCalculator.java
@@ -6,7 +6,7 @@ public class StringCalculator {
             return 0;
         }
 
-        String[] parts = numbers.split(",");
+        String[] parts = numbers.split(",|\n");
         int sum = 0;
         for (String part : parts) {
             sum += Integer.parseInt(part);
