b9e4967b24af86c3e20810daa8de9462debcee4f
meichflo <meichflo@students.zhaw.ch>
2024-04-29 10:56:39 +0200

    Add support for custom delimiters in StringCalculator

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/StringCalculator.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/StringCalculator.java
@@ -9,7 +9,15 @@ public class StringCalculator {
             return 0;
         }
     
-
+        String delimiter = ",|\n";
+        if (numbers.startsWith("//")) {
+            int delimiterStart = numbers.indexOf("//") + 2;
+            int delimiterEnd = numbers.indexOf("\n");
+            delimiter = numbers.substring(delimiterStart, delimiterEnd);
+            delimiter = delimiter.substring(1, delimiter.length() - 1); // Remove the brackets
+            delimiter = delimiter.replace("][", "|"); // Replace ][ with | to create a regex pattern
+            numbers = numbers.substring(numbers.indexOf("\n") + 1);
+        }
         
         String[] parts = numbers.split(delimiter);
         int sum = 0;
